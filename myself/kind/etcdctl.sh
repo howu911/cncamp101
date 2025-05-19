2984056d2471057751f0bc49f7231b00158d7152
howu911 <496272917@qq.com>
2025-05-19 22:14:26 +0800

    kind

---
+++ myself/kind/etcdctl.sh
@@ -0,0 +1,9 @@
+export ETCDCTL_API=3
+export ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt
+export ETCDCTL_CERT=/etc/kubernetes/pki/etcd/server.crt
+export ETCDCTL_KEY=/etc/kubernetes/pki/etcd/server.key
+export ETCDCTL_ENDPOINTS=https://127.0.0.1:2379
+
+etcdctl endpoint health
+
+
